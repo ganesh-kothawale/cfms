dbf8040eaed8b02345795a42d315cb330a452276
Zenith2526 <zenith.surapaneni@theporter.in>
2024-09-17 11:01:52 +0530

    removing unnecessary properties

--- servers/commons/src/main/kotlin/in/porter/cfms/servers/commons/di/ApplicationConfig.kt
+++ servers/commons/src/main/kotlin/in/porter/cfms/servers/commons/di/ApplicationConfig.kt
@@ -15,21 +15,8 @@ object ApplicationConfig {
   private val properties = Properties().loadFile(Dir.PROPERTIES,"application.properties")
 
   val env: Environment = Environment.valueOf(properties.getProperty("env"))
-  val queueUrl: String = properties.getProperty("queue_url")
-  val redisUrl: String = properties.getProperty("redis_url")
-
-  val externalHost: String = properties.getProperty("external.host")
-  val externalPort: Int = properties.getProperty("external.port").toInt()
-  val externalProtocol: URLProtocol = URLProtocol(properties.getProperty("external.protocol"), externalPort)
-
-  val jobsProvider: String = properties.getProperty("jobs.provider", "sqs")
 
   val tasksProject: String = properties.getProperty("jobs.tasks.project")
-  val tasksLocation: String = properties.getProperty("jobs.tasks.location")
-  val tasksQueue: String = properties.getProperty("jobs.tasks.queue")
-  val tasksProjectEmail: String = properties.getProperty("jobs.tasks.service_account_mail")
-
-  val pubsubTopic: String = properties.getProperty("jobs.pubsub.topic")
 
   val pubsubSubscription: String = properties.getProperty("jobs.pubsub.subscription")
 }
