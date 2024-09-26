60dd79baf33c29ed0b380542edfb0bcd302872e9
rahilvijay <110456714+rahilvijay@users.noreply.github.com>
2024-09-26 22:17:27 +0530

    refactored the http service

--- servers/ktor/src/main/kotlin/in/porter/cfms/servers/ktor/usecases/orders/FetchOrdersHTTPService.kt
+++ servers/ktor/src/main/kotlin/in/porter/cfms/servers/ktor/usecases/orders/FetchOrdersHTTPService.kt
@@ -1,28 +1,28 @@
-package `in`.porter.cfms.servers.ktor.usecases.orders
-
+import com.google.api.Logging
 import `in`.porter.cfms.api.models.orders.FetchOrderApiRequest
-import `in`.porter.cfms.api.models.orders.FetchOrderResponse
 import `in`.porter.cfms.api.service.orders.usecases.FetchOrdersApiService
 import `in`.porter.kotlinutils.instrumentation.opentracing.Traceable
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import org.apache.logging.log4j.kotlin.Logging
-import io.ktor.http.HttpStatusCode // Correct import
+import io.ktor.http.HttpStatusCode
 import javax.inject.Inject
 
 class FetchOrdersHTTPService @Inject constructor(
     private val fetchOrdersApiService: FetchOrdersApiService
 ) : Traceable {
 
-    companion object : Logging
 
     suspend fun invoke(call: ApplicationCall) {
         trace {
             try {
-                call.receive<FetchOrderApiRequest>()
+                val page = call.request.queryParameters["page"]?.toIntOrNull() ?: 1
+                val limit = call.request.queryParameters["limit"]?.toIntOrNull() ?: 10
+
+                FetchOrderApiRequest(page = page, limit = limit)
                     .let { fetchOrdersApiService.invoke(it) }
                     .let { call.respond(HttpStatusCode.OK, it) }
+
             } catch (e: IllegalArgumentException) {
                 call.respond(HttpStatusCode.BadRequest, "Invalid request: ${e.message}")
             } catch (e: Exception) {
