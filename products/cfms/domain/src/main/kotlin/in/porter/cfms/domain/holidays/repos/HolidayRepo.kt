7dd577855b738c2833855f9866b118a28931c6b6
ganesh-kothawale-porter <ganesh.kothawalr@theporter.in>
2024-10-01 07:35:38 +0530

    Implemented Update holiday API domain layer

--- products/cfms/domain/src/main/kotlin/in/porter/cfms/domain/holidays/repos/HolidayRepo.kt
+++ products/cfms/domain/src/main/kotlin/in/porter/cfms/domain/holidays/repos/HolidayRepo.kt
@@ -1,6 +1,7 @@
 package `in`.porter.cfms.domain.holidays.repos
 
 import `in`.porter.cfms.domain.holidays.entities.Holiday
+import `in`.porter.cfms.domain.holidays.entities.UpdateHolidayEntity
 import java.time.LocalDate
 
 interface HolidayRepo {
@@ -8,4 +9,8 @@ interface HolidayRepo {
     suspend fun getByIdAndDate(franchiseId: String, startDate: LocalDate, endDate: LocalDate): Holiday?
 
     suspend fun record(request: Holiday) : Long
+
+    suspend fun update(request: UpdateHolidayEntity) : Long
+
+    suspend fun getById(holidayId: Long): UpdateHolidayEntity?
 }
