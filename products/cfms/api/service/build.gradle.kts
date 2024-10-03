65b6737bd2140642ff65d28c837e2f4743c9b688
shivamrajPorter <shivam.raj@theporter.in>
2024-10-03 19:29:09 +0530

    local config for psql

--- products/cfms/api/service/build.gradle.kts
+++ products/cfms/api/service/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
   implementation(project(Modules.CFMS.domain))
   implementation(project(Modules.CFMS.Api.models))
   implementation(Libs.KotlinUtils.openTracing)
+  testImplementation(Libs.Testing.testContainersPostgresql)
 }
