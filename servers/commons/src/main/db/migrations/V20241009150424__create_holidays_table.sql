3f5f03971d9b73fa613d612dfbcf46c0212ace74
aakashkundnani <aakashkumar.kundnani@theporter.in>
2024-10-09 17:42:59 +0530

    PR Review Feedback
    In the DB migration, add length of strings - Solved
    Also add uniqueness constraint to email id. - Solved

--- servers/commons/src/main/db/migrations/V20241009150424__create_holidays_table.sql
+++ servers/commons/src/main/db/migrations/V20241009150424__create_holidays_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "holidays" (
   "id" SERIAL PRIMARY KEY,
-  "franchise_id" varchar,
-  "start_date" date,
-  "end_date" date,
-  "holiday_name" varchar,
-  "leave_type" varchar CHECK ("leave_type" IN ('Normal', 'Emergency')) NOT NULL,
-  "backup_franchise_ids" varchar,
-  "created_at" timestamp DEFAULT now(),
-  "updated_at" timestamp DEFAULT now()
+  "franchise_id" VARCHAR(10),
+  "start_date" DATE,
+  "end_date" DATE,
+  "holiday_name" VARCHAR(50),
+  "leave_type" VARCHAR(10) CHECK ("leave_type" IN ('Normal', 'Emergency')) NOT NULL,
+  "backup_franchise_ids" VARCHAR(10),
+  "created_at" TIMESTAMP DEFAULT NOW(),
+  "updated_at" TIMESTAMP DEFAULT NOW()
 );
