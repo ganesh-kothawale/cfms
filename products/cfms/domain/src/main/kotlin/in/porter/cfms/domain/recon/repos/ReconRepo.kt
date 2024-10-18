f4ddbfc579831aee7c0885fb67728b934b1e780a
ganesh-kothawale-porter <ganesh.kothawalr@theporter.in>
2024-10-18 18:50:18 +0530

    Implemented Delete recon shadow API functionality

--- products/cfms/domain/src/main/kotlin/in/porter/cfms/domain/recon/repos/ReconRepo.kt
+++ products/cfms/domain/src/main/kotlin/in/porter/cfms/domain/recon/repos/ReconRepo.kt
@@ -6,4 +6,6 @@ interface ReconRepo {
     suspend fun findAllRecons(page: Int, size: Int): List<Recon>
     suspend fun countAllRecons(): Int
     suspend fun create(recon: Recon): String
+    suspend fun findReconById(reconId: String): Recon?  // Find recon by ID
+    suspend fun deleteReconById(reconId: String)  // Delete recon by ID
 }
