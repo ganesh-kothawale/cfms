65077c02f11ecf6a78e3bbb15847d7a2042bf217
dasoju-pranay <dasoju.kumar@theporter.in>
2024-10-18 05:36:59 +0530

    inprog

---
+++ products/cfms/api/models/src/main/kotlin/in/porter/cfms/api/models/hlp/RecordFranchiseDetailsResponse.kt
@@ -0,0 +1,16 @@
+package `in`.porter.cfms.api.models.hlp
+
+data class RecordFranchiseDetailsResponse(
+    val data: Data? = null,
+    val error: List<ErrorResponse> = emptyList()
+)
+
+data class Data(
+    val message: String,
+    val hlpOrderId: String
+)
+
+data class ErrorResponse(
+    val message: String,
+    val details: String
+)
