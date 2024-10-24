0ec457cca4569c90d1761c2afba80ad06d73c840
aakashkundnani <aakashkumar.kundnani@theporter.in>
2024-10-24 18:54:31 +0530

    Variable Name change
    Variable Name change

--- products/cfms/domain/src/main/kotlin/in/porter/cfms/domain/exceptions/FranchiseAlreadyExistsException.kt
+++ products/cfms/domain/src/main/kotlin/in/porter/cfms/domain/exceptions/FranchiseAlreadyExistsException.kt
@@ -1,4 +1,4 @@
 package `in`.porter.cfms.domain.exceptions
 
-class FranchiseAlreadyExistsException(code: String) :
-  FranchiseException("Franchise with Franchise email $code already exists")
+class FranchiseAlreadyExistsException(email: String) :
+  FranchiseException("Franchise with Franchise email $email already exists")
