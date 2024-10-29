132951a0df44de3ae93fee5a96c5c9a87bc6ae5f
dasoju-pranay <dasoju.kumar@theporter.in>
2024-10-29 14:16:46 +0530

    remove redundant http service files

--- servers/ktor-external/src/main/kotlin/in/porter/cfms/servers/ktor/external/app/MainApplication.kt
+++ servers/ktor-external/src/main/kotlin/in/porter/cfms/servers/ktor/external/app/MainApplication.kt
@@ -13,11 +13,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import `in`.porter.cfms.servers.ktor.external.usecases.holidays.holidaysRoutes
-import `in`.porter.cfms.servers.ktor.external.usecases.franchises.franchiseRoutes
-import `in`.porter.cfms.servers.ktor.external.usecases.recon.reconRoutes
-import `in`.porter.cfms.servers.ktor.external.usecases.tasks.tasksRoutes
-import `in`.porter.cfms.servers.ktor.external.usecases.pickuptasks.pickupTasksRoutes
 import io.ktor.server.application.*
 import io.ktor.http.*
 import io.ktor.server.response.*
@@ -97,10 +92,5 @@ fun Application.main() {
 
     routing {
         get("/") { call.respond(HttpStatusCode.OK, Unit) }
-        route ("/cfms/public/holidays"){ holidaysRoutes(httpComponent) }
-        route("/cfms/public/franchises") { franchiseRoutes(httpComponent) }
-        route("/cfms/public/tasks") { tasksRoutes(httpComponent) }
-        route("/cfms/public/recon") { reconRoutes(httpComponent) }
-        route("/cfms/public/pickup_tasks") { pickupTasksRoutes(httpComponent) }
     }
 }
